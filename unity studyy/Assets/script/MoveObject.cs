62fbc69d4bc0b8f6ca04a289c56d686e706e5eb4
박민규 <41883207+mingyu8789@users.noreply.github.com>
2025-03-10 15:06:17 +0900

    1

--- unity studyy/Assets/script/MoveObject.cs
+++ unity studyy/Assets/script/MoveObject.cs
@@ -9,8 +9,27 @@ public class MoveObject : MonoBehaviour
   
     void Update()
     {
-        //키 입력에 따라 이동
-        float move = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * move * speed * Time.deltaTime);
+
+        ////키 입력에 따라 이동
+        //float move = Input.GetAxis("Horizontal");
+        //// 방향 * 스피드 * 타임델타타임
+        //transform.Translate(Vector3.right * move * speed * Time.deltaTime);
+
+
+
+
+        ////위아래 옆까지 움직임
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        //transform.position += move * speed * Time.deltaTime;
+
+
+
+        //위아래 앞뒤까지 움직임
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //transform.position += move * speed * Time.deltaTime;  //회전영향 없이 움직일 수 있음
+        transform.Translate(move * speed * Time.deltaTime);     //회전 방향에 따라 움직임이 달라짐
+
     }
 }
---
+++ unity studyy/Assets/script/VectorStudy.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class VectorStudy : MonoBehaviour
+{
+
+    //public Vector2 v2 = new Vector2(10,10); 
+    //public Vector3 v3 = new Vector3(1,1,1);
+    
+
+
+
+    void Start()
+    {
+        //Vector3 a = new Vector3(1, 0, 0);
+        //Vector3 b = new Vector3(2, 0, 0);
+
+        //Vector3 c = a + b;
+
+        //Debug.Log("Vactor" + c);
+
+
+
+
+        //Vector3 a = new Vector3(1, 1, 0);
+        //Vector3 b = new Vector3(2, 0, 0);
+
+        //Vector3 c = a + b;
+
+        //Debug.Log("Vactor" + c);
+
+        //Debug.Log("길이 : " + c.magnitude);
+
+
+
+        //정규화 normalize
+        //백터의 크기를 1로 만들고 방향만 유지
+        Vector3 a = new Vector3(3, 0, 0);
+
+        Vector3 normalizedVector = a.normalized;
+
+        Debug.Log("1의길이 방향만 설정하는 정규화 : " + normalizedVector);
+
+
+
+
+
+
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
